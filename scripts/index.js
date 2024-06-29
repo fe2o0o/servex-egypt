76c98ae2b6c19a713ff9fbd075f79b2fe19d505d
ibrahim <ibrahimfe2o0o@gmail.com>
2024-06-29 21:10:24 +0300

    Servex-Egypt Web Site

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#loader").fadeOut(1000);
+  AOS.init();
+  document.querySelectorAll(".nav-link").forEach((e) => {
+    const urlLink = window.location.pathname;
+    e.classList.remove("active");
+    if (e.getAttribute("href") == urlLink) {
+      e.classList.add("active");
+    }
+  });
+});
